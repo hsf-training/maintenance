e3d1288b6b5778de000a86f9cd5612d0545ef51c
Kilian Lieret <kilian.lieret@posteo.de>
2022-07-13 15:44:26 -0400

    Add code to switch to jekyll theme

---
+++ move-to-theme/README.md
@@ -0,0 +1,4 @@
+# Move to theme
+
+Make all lesson repositories use the Jekyll theme instead of carrying a duplicate
+of the theme files.
---
+++ move-to-theme/remove-theme-files.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+IFS=$'\n\t'
+
+rm -r _includes
+rm -r _layouts
+rm -r assets
+rm -r bin
+rm -rf _episodes_rmd
+
+# Remove folders that only contain ".gitkeep"
+if [[ $(ls data|wc -l|xargs) == "0" ]]; then rm -r data; fi
+if [[ $(ls code|wc -l|xargs) == "0" ]]; then rm -r code; fi
+if [[ $(ls files|wc -l|xargs) == "0" ]]; then rm -r files; fi
