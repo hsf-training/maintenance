f15fa90288e309d97a5dd80b24cc31b222499f36
Kilian Lieret <kilian.lieret@posteo.de>
2022-07-14 13:13:15 -0400

    Do not print index

--- repository_list/get_lesson_repos.py
+++ repository_list/get_lesson_repos.py
@@ -5,4 +5,4 @@ import pandas as pd
 
 df = pd.read_csv("repositories.csv")
 df = df[df.is_carpentry_lesson].copy()["repository"]
-df.to_csv("lesson_repositories.txt", header=False)
+df.to_csv("lesson_repositories.txt", header=False, index=False)
