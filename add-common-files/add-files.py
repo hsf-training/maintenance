004128239255264660ffc6ee393575af2633607c
Kilian Lieret <kilian.lieret@posteo.de>
2022-09-06 12:16:34 -0400

    ✨ Allow to partially replace file
    Fixes #14

--- add-common-files/add-files.py
+++ add-common-files/add-files.py
@@ -50,12 +50,44 @@ files_to_add = [
     ),
 ]
 
+SHARED_START_LINE_MARKER = "CENTRALLY MAINTAINED FILE"
+SHARED_STOP_LINE_MARKER = "END CENTRALLY MAINTAINED FILE"
+
 
 def has_common_header(txt: str) -> bool:
     lines = txt.splitlines()
     if not lines:
         return False
-    return "CENTRALLY MAINTAINED FILE" in lines[0]
+    return SHARED_START_LINE_MARKER in lines[0]
+
+
+def get_text_between_marker(
+    txt: str, start_marker: str, end_marker: str
+) -> tuple[str, str, str]:
+    """
+
+    Returns:
+         before, middle, after
+    """
+    before = []
+    after = []
+    middle = []
+    part = 0
+    for line in txt.splitlines():
+        if end_marker in line:
+            # Must come first, as end_marker is also in start_marker
+            assert part == 1
+            part = 2
+        elif start_marker in line:
+            part = 1
+
+        if part == 0:
+            before.append(line)
+        elif part == 1:
+            middle.append(line)
+        elif part == 2:
+            after.append(line)
+    return "\n".join(before), "\n".join(middle), "\n".join(after)
 
 
 def add_file(sf: SharedFile) -> None:
@@ -67,6 +99,19 @@ def add_file(sf: SharedFile) -> None:
         _target.parent.mkdir(exist_ok=True, parents=True)
         shutil.copy(_source, _target)
 
+    def replace_shared_part(_source: Path, _target: Path) -> None:
+        s_before, s_middle, s_after = get_text_between_marker(
+            _source.read_text(),
+            start_marker=SHARED_START_LINE_MARKER,
+            end_marker=SHARED_STOP_LINE_MARKER,
+        )
+        t_before, _, t_after = get_text_between_marker(
+            _target.read_text(),
+            start_marker=SHARED_START_LINE_MARKER,
+            end_marker=SHARED_STOP_LINE_MARKER,
+        )
+        _target.write_text(f"{t_before}\n{s_middle}\n{t_after}")
+
     if target.is_file():
         if sf.exist_action == ExistAction.OVERWRITE:
             return replace_file(source, target)
@@ -74,7 +119,7 @@ def add_file(sf: SharedFile) -> None:
             sf.exist_action == ExistAction.OVERWRITE_IF_HEADER
             and has_common_header(target.read_text())
         ):
-            return replace_file(source, target)
+            return replace_shared_part(source, target)
         elif sf.exist_action == ExistAction.SKIP:
             print(f"Skipping {sf.path}")
             return
